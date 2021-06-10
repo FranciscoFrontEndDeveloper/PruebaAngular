267965d04202a0383cd70d605db36ce8944e2087
FranciscoFrontEndDeveloper <pachonetworking@gmail.com>
2021-06-10 16:18:57 -0500

    Components test developer angular created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfferComponent } from './Components/offer/offer.component';
+import { CharacteristicsComponent } from './Components/characteristics/characteristics.component';
+import { PricesComponent } from './Components/prices/prices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfferComponent,
+    CharacteristicsComponent,
+    PricesComponent
   ],
   imports: [
     BrowserModule,
