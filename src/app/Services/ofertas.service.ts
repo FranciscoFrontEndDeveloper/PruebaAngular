cb136777eafe14b09f2324a6c4a4b847122391a2
FranciscoFrontEndDeveloper <pachonetworking@gmail.com>
2021-06-10 16:49:39 -0500

    array ofertas created

--- src/app/Services/ofertas.service.ts
+++ src/app/Services/ofertas.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { OFERTAS } from '../ofertas/ofertas.db';
 @Injectable({
   providedIn: 'root'
 })
 export class OfertasService {
 
   constructor() { }
+
+  getAllOfertas(): any{
+    return OFERTAS;
+  }
 }
